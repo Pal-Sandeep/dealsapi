e674b30d6f78d54ec17659729ddab61e0cb636a7
Sandeep Pal <skumar8work@gmail.com>
2024-02-20 20:32:59 +0530

    vercel config added

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = [".vercel.app", ".render.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
